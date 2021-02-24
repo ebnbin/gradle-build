e2d6f0f246467163ca66ebb9a0458c192c65c833
Ebn Zhang <ebnbin@gmail.com>
2021-02-24 17:28:31 +0800

    Update extra.gradle.kts

--- extra.gradle.kts
+++ extra.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.extra["dependencyMap"] = mapOf(
     "com.android.tools.build:gradle" to "4.0.2", // https://developer.android.com/studio/releases/gradle-plugin https://maven.google.com/web/index.html
     "org.jetbrains.kotlin:kotlin-gradle-plugin" to "1.4.30", // https://github.com/jetbrains/kotlin
-    "androidx.core:core-ktx" to "1.3.2" // https://developer.android.com/jetpack/androidx/versions
+    "androidx.core:core-ktx" to "1.3.2", // https://developer.android.com/jetpack/androidx/versions
+    "androidx.recyclerview:recyclerview" to "1.1.0"
 )
