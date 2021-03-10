34c091067d39c31feb3c23451ddd5742a2b05c64
Ebn Zhang <ebnbin@gmail.com>
2021-03-10 18:36:36 +0800

    Update

--- src/main/kotlin/Dependencies.kt
+++ src/main/kotlin/Dependencies.kt
@@ -9,11 +9,11 @@ object Dependencies {
     )
     val comGithubEbnbin_ebdev = DevDependency(
         name = "ebdev",
-        version = "0.0.3"
+        version = "0.0.4"
     )
     val comGithubEbnbin_openmojiPicker = DevDependency(
         name = "openmoji-picker",
-        version = "0.0.18"
+        version = "0.0.19"
     )
 
     val comAndroidToolsBuild_gradle = Dependency(
